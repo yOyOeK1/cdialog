59aab93977a865d9d7cfb2bbf40d45424075ffbd
Bartłomiej Ceglik <yoyoek@wp.pl>
2013-01-18 09:00:59 +0000

    fix home paths...

--- ccanvasAsBar.sh
+++ ccanvasAsBar.sh
@@ -4,4 +4,4 @@ echo '{"version": 1, "click_events":true }'
 echo '['
 echo '[]'
 
-exec /home/yoyo/Apps/cdialog/ccanvash.bin -asBar $* | grep -e 'full_text'
+exec $HOME/Apps/cdialog/ccanvash.bin -asBar $* | grep -e 'full_text'
