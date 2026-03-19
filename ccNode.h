3f1e580e113554ba32eee85a93285d14b811d20b
Bartłomiej Ceglik <yoyoek@wp.pl>
2026-03-19 17:31:51 +0000

    ccanvas can color but how to put it with correct character count ?

--- ccNode.h
+++ ccNode.h
@@ -3,11 +3,13 @@ struct ccNode{
 	int id;
 	int pos[2];
 	int size[2];
-	char fbColor[4];
+	int fbColor[4];
 	char bgColor[4];
 	char name[512];
 	char text[512];
 	char fUpdate[128];
+	bool tRenderDone;
+	char tRender[1024];
 	int entryDate;
 
 };
