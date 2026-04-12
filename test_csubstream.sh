85f97ec26aeee73c83552cf2083d38b7b12f8210
Bartłomiej Ceglik <yoyoek@wp.pl>
2026-04-12 17:04:36 +0000

    add csubstream.c to have live stream of childrem process

---
+++ test_csubstream.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+date
+echo "#* ... test of c sub process stream "
+sleep 1
+echo 1
+sleep 1
+echo 2
+sleep 1
+echo 3
+sleep 1
+cal
+sleep 1
+date
+echo "DONE.............. exit 0"
+exit 0
