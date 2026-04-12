28a0b7ef8cbf55920f75004cd208f664cf8d1359
Bartłomiej Ceglik <yoyoek@wp.pl>
2026-04-12 17:42:17 +0000

    csubstream sese test state and can do some from struct so semi object :P

--- csubstream.c
+++ csubstream.c
@@ -4,38 +4,50 @@
 #include <sys/wait.h>
 #include <string.h>
 
-int main() {
-    int pipefd[2];
-    pid_t pid;
-    char buffer[1024];
+struct csubexec{
+	char cmd[512];
+	int pipefd[2];
+	pid_t pid;
+	char buffer[1024];
+	int linesC;
+	int status;
+};
 
-    // 1. Create the pipe: pipefd[0] is read, pipefd[1] is write
-    if (pipe(pipefd) == -1) {
+
+int csubstream( struct csubexec csube ) {
+    printf("csubstream ... with ... cmd[%s]\n", csube.cmd );
+    //int pipefd[2];
+    //pid_t pid;
+    //char buffer[1024];
+
+    // 1. Create the pipe: csube.pipefd[0] is read, csube.pipefd[1] is write
+    if(pipe(csube.pipefd) == -1) {
         perror("pipe");
         exit(EXIT_FAILURE);
     }
 
-    pid = fork();
+    csube.pid = fork();
 
-    if (pid == -1) {
+    if(csube.pid == -1) {
         perror("fork");
         exit(EXIT_FAILURE);
     }
 
-    if (pid == 0) {
+    if(csube.pid == 0) {
         // --- CHILD PROCESS ---
         // Close the read end of the pipe (not needed by child)
-        close(pipefd[0]);
+        close(csube.pipefd[0]);
 
         // Redirect stdout to the write end of the pipe
-        dup2(pipefd[1], STDOUT_FILENO);
+        dup2(csube.pipefd[1], STDOUT_FILENO);
         // Optional: Redirect stderr to stdout to capture errors too
-        dup2(pipefd[1], STDERR_FILENO);
+        dup2(csube.pipefd[1], STDERR_FILENO);
 
-        close(pipefd[1]);
+        close(csube.pipefd[1]);
 
         // Execute bash command
-        execlp("sh", "sh", "-c", "termux-sensor -s orientation -d 1000", NULL);
+        execlp("sh", "sh", "-c", csube.cmd, NULL);
+        //execlp("sh", "sh", "-c", "termux-sensor -s orientation -d 1000", NULL);
         //execlp("sh", "sh", "-c", "./test_csubstream.sh", NULL);
         //execlp("sh", "sh", "-c", "ls -l /etc | head -n 5", NULL);
         
@@ -45,37 +57,57 @@ int main() {
 
     } else {
         // --- PARENT PROCESS ---
-        int status;
+        //int status;
 
         // Close the write end of the pipe (not needed by parent)
-        close(pipefd[1]);
+        close(csube.pipefd[1]);
 
         // Convert the file descriptor to a FILE stream for easy line reading
-        FILE *stream = fdopen(pipefd[0], "r");
-        if (stream == NULL) {
+        FILE *stream = fdopen(csube.pipefd[0], "r");
+        if(stream == NULL) {
             perror("fdopen");
             exit(EXIT_FAILURE);
         }
 
         printf("--- Capturing Output ---\n");
-        while (fgets(buffer, sizeof(buffer), stream) != NULL) {
+        while (fgets(csube.buffer, sizeof(csube.buffer), stream) != NULL) {
             // Process the line in the buffer
-            printf("Buffer: %s", buffer);
+            printf("[%03i] Buffer: %s", csube.linesC++, csube.buffer );
         }
 
         fclose(stream);
-        close(pipefd[0]);
+        close(csube.pipefd[0]);
 
         // 2. Handle Exit Code
-        waitpid(pid, &status, 0);
+        waitpid(csube.pid, &csube.status, 0);
 
-        if (WIFEXITED(status)) {
-            int exit_code = WEXITSTATUS(status);
+        if(WIFEXITED(csube.status)) {
+            int exit_code = WEXITSTATUS(csube.status);
             printf("--- Child exited with code: %d ---\n", exit_code);
-        } else if (WIFSIGNALED(status)) {
-            printf("--- Child terminated by signal: %d ---\n", WTERMSIG(status));
+        } else if(WIFSIGNALED(csube.status)) {
+            printf("--- Child terminated by signal: %d ---\n", WTERMSIG(csube.status));
         }
     }
 
     return 0;
 }
+
+
+int main(){
+
+	printf("#* ... csubstream ... START\n");
+
+	struct csubexec cse0 = {
+		.cmd = "whoami",
+	};
+	csubstream( cse0 );	
+	
+
+	struct csubexec cse1 = {
+		.cmd = "./test_csubstream.sh"
+	};
+	csubstream( cse1 );	
+	printf("#* ... csubstream ... DONE\n");
+	return 0;
+}
+
