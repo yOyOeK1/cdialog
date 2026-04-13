1fb1154fec30bdf2f463785c0c15a3d12928825d
Bartłomiej Ceglik <yoyoek@wp.pl>
2026-04-13 07:58:00 +0000

    csubstream can now do test have some basic struct

--- csubstreamBuild.sh
+++ csubstreamBuild.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-gcc ./csubstream.c -o csubstream.test.bin && \
+gcc ./csubstream.c -o csubstream.test.bin \
+	-DCSUBSTREAMTEST \
+	&& \
 	echo "build OK" && \
 	ls -alh ./csubstream.test.bin && ./csubstream.test.bin $* 
