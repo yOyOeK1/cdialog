c488d48f0415b340857c37ad9dc8d6ca5c3133c2
Bartłomiej Ceglik <yoyoek@wp.pl>
2026-04-15 09:07:07 +0000

    mqttView2 to use ccanvas and ccanvas as test now build or include update ccanvas .h file

--- ccanvas.h
+++ ccanvas.h
@@ -1,14 +1,22 @@
 #include "ccNode.h"
 
-int *widgetTick(  struct ccNode *cN, int frameNo );
+/* auto header for [ ./ccanvas.c ] */
+
 int *fUpdateDef( struct ccNode *cN, int frameNo );
+int *widgetTick(  struct ccNode *cN, int frameNo );
+void *dogBatteryLooper( void *vargp );
+void *dogTimeLooper( void *vargp );
 void *dogLooper( void *vargp );
+void dogsStart();
+void dogsStop();
 int ccInit_FB();
 int ccFree_FB();
 int ccInit();
+char *cc_getPx( int x, int y );
 int ccUpdate();
+int cc_clear( char cBlank );
 int cc_printf( int x, int y, char *msg );
 int ccDraw();
-char *ccGetPX( int x, int y );
-void ccClear( char cBlank );
 void ccRender();
+
+
