b8b1d549fab1abfa8434447dd9941a3fb2d86c2e
Bartłomiej Ceglik <yoyoek@wp.pl>
2026-04-17 07:36:15 +0000

    ctermh have now a function to get size of term ctermSize :)

--- ctermh.c
+++ ctermh.c
@@ -2,16 +2,29 @@
 #include <unistd.h>
 #include <sys/ioctl.h>
 
-int main(){
+
+void ctermSize( int *col, int *row ){
 	struct winsize w;
+	ioctl( STDOUT_FILENO, TIOCGWINSZ, &w );
+	*col = w.ws_col;
+	*row = w.ws_row;
+}
 
+
+#ifdef CTERMTEST
+
+int main(){
+
+	struct winsize w;
 	ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+	printf("This terminal window is %d rows by %d columns\n", w.ws_row, w.ws_col );
 
-	printf("This terminal window is %d rows by %d columns\n",
-			w.ws_row,
-			w.ws_col
-		  );
+	int mc;
+	int mr;
+	ctermSize( &mc, &mr );
+	printf("2 This terminal window is %d rows by %d columns\n", mc, mr  );
 
 	return(0);
 }
 
+#endif
