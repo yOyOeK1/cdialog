39793e581cd2e60deca13871ede4cdc0f5f370b2
Bartłomiej Ceglik <yoyoek@wp.pl>
2026-04-23 08:29:49 +0000

    cnn_config for external sharing of structs ....

---
+++ cnn_config_data.h
@@ -0,0 +1,10 @@
+
+#ifndef CNN_DATA_H
+#define CNN_DATA_H
+
+#include "cnn_config_types.h"
+
+extern MqHost MqHosts[];
+extern int MqHostsCount;
+
+#endif
---
+++ cnn_config_types.h
@@ -0,0 +1,12 @@
+
+#ifndef CNN_TYPES_H
+#define CNN_TYPES_H
+
+
+typedef struct {
+	int id;
+	char name[512]; 
+
+} MqHost;
+
+#endif
