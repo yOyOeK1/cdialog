faecdc64fb8b0231ce7932184681f6ed9b38668c
Bartłomiej Ceglik <yoyoek@wp.pl>
2026-04-24 11:48:34 +0000

    cmachine2 keyBind separation not yet

--- cmachine2.c
+++ cmachine2.c
@@ -279,6 +279,38 @@ char *key_getCurrentMode_name(){
 	return "NaN";
 }
 
+void keyBindDoIt(){
+	keyNo = 0;
+	keyCmdOk = false;
+	while( true ){
+		printf("[q] key to exit ...\n");
+		keyCh = getchar();
+
+		if ( keyCh == '\n' ){
+			keyCmdOk = true;
+			keyIn[ keyNo ] = 0;
+		} else {
+			keyIn[ keyNo++ ] = keyCh;
+		}
+
+		if( keyCmdOk ){
+			printf("CMD@[%i:%s] [%s] (%i)\n", cnn_KeyModeNow, key_getCurrentMode_name(), keyIn, keyNo );
+			
+			if( keyNo == 1 && keyIn[0] == 'q' ){
+				printf("Exit by q\n");
+				break;
+			}else if( keyNo == 2 && keyIn[0] == 'm' ){
+				sscanf( keyIn, "m%li",  &cnn_KeyModeNow );
+				printf("Mode switch ...[%i:%s]\n", cnn_KeyModeNow, key_getCurrentMode_name() );
+			}  	
+
+			keyCmdOk = false;
+			keyNo = 0;
+
+		}
+	}
+
+}
 
 int main( int argc, char *argv[] ){
 	if( argc > 1 && cc_main_argcParse( argc, argv )!= 1 )  return 0;
@@ -307,40 +339,7 @@ int main( int argc, char *argv[] ){
 	mqttInit2( &cnn_mqtt_on_message );
 	mqttDoIt2();
 
-	keyNo = 0;
-	keyCmdOk = false;
-	while( true ){
-		printf("Any key to exit ...\n");
-		keyCh = getchar();
-	
-
-		if ( keyCh == '\n' ){
-			keyCmdOk = true;
-			keyIn[ keyNo ] = 0;
-		} else {
-			keyIn[ keyNo++ ] = keyCh;
-		}
-
-
-		printf("keys [%s]\n", keyIn );
-
-
-		if( keyCmdOk ){
-			printf("CMD@[%i:%s] [%s] (%i)\n", cnn_KeyModeNow, key_getCurrentMode_name(), keyIn, keyNo );
-			
-			if( keyNo == 1 && keyIn[0] == 'q' ){
-				printf("Exit by q\n");
-				break;
-			}else if( keyNo == 2 && keyIn[0] == 'm' ){
-				sscanf( keyIn, "m%li",  &cnn_KeyModeNow );
-				printf("Mode switch ...\n");
-			}  	
-
-			keyCmdOk = false;
-			keyNo = 0;
-
-		}
-	}
+	keyBindDoIt();
 	
 	//mqttInit();
 	//mqttDoIt();
