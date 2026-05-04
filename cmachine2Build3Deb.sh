5d2c35b8007427a505ad1e4cb21ab1c3fb761425
Bartłomiej Ceglik <yoyoek@wp.pl>
2026-05-04 08:18:26 +0000

    update build2 for cmachine for debug

--- cmachine2Build3Deb.sh
+++ cmachine2Build3Deb.sh
@@ -3,7 +3,8 @@
 #
 
 bTarget="cmachine2.test.bin"
-bSrc="cnn_config_data.c ckeyh.c cmdh.c ctermh.c ccanvas.c cargs.c cpostprocess.c mqtth.c cmachine2.c"
+#bSrc="cnn_config_data.c ckeyh.c cmdh.c ctermh.c ccanvas.c cargs.c cpostprocess.c mqtth.c cmachine2.c"
+bSrc="cnn_config_data.c cmTools.c cmInits.c cmCanvas.c cmMath.c cmAs.c cmLogic.c cmTime.c timeh.c ckeyh.c cmdh.c ctermh.c ccanvas.c cargs.c cpostprocess.c mqtth.c cmachine2.c"
 inc=""
 libsDir=""
 libs="-lm"
